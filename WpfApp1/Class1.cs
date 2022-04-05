2d4558af80f126bd234af89225e3ddbd4b3a9734
Vlastimil Vašut <Vasut.Vlastimil@stud.vassboskovice.cz>
2022-04-05 11:33:40 +0200

    Přidejte soubory projektu.

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Kostka
+    {
+        private int hodnota;
+
+        public int Hodnota { get => hodnota; }
+        public void Hod()
+        {
+            Random random = new Random();
+            hodnota = random.Next(1, 7);
+        }
+    public Kostka()
+        {
+            Hod();
+        }
+ 
+    
+    }
+ 
+
+}
+
